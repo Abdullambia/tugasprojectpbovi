a3ed2aa19a12d7ef81da43a8dd338d516256a908
Abdullambia <153804597+Abdullambia@users.noreply.github.com>
2024-01-14 20:30:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import mysql.connector
+
+con = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "",
+    database = "5220411421"
+)    
+if con.is_connected():
+    print("Koneksi ke server database berhasil")
+
+db = con.cursor()
+# db.execute("create table if not exists tbmahasiswa(Nim varcar(10),"
+#            "Nama_Mahasiswa varchar(50), Alamat varchar(50), Prodi varchar(50), Angkatan Varchar(250))")  
+
 
